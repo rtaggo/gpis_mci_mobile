7830a859facc1631bd438da849a0002b97971fe6
LauraJeandenans <52075148+LauraJeandenans@users.noreply.github.com>
2019-09-05 14:39:26 +0200

    Update galigeo.js

--- public/scripts/galigeo/galigeo.js
+++ public/scripts/galigeo/galigeo.js
@@ -218,6 +218,26 @@
           }
         });
       }
+    } else if (typeof patrouilleId == 'undefined') {
+      if (options.fonction == 'fin') {
+        const patrouillesUrl = `${options.baseRESTServicesURL}/fin_vacation.php?patrouille=${options.userName}`;
+        $.ajax({
+          type: 'GET',
+          url: patrouillesUrl,
+          success: function(response) {
+            console.log(`Revoke Patrouille response: `, response);
+            GGO.postLogoutForm();
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            if (textStatus === 'abort') {
+              console.warn(`${patrouillesUrl} Request aborted`);
+            } else {
+              console.error(`Error for ${patrouillesUrl} request: ${textStatus}`, errorThrown);
+            }
+            GGO.postLogoutForm();
+          }
+        });
+      }
     } else if (typeof options.userRole !== 'undefined' && (options.userRole === 'charly' || options.userRole === 'alpha')) {
       console.warn(`Might be usefull to disconnect the user ${options.userName} with role ${options.role} from the system`);
       GGO.postLogoutForm();
