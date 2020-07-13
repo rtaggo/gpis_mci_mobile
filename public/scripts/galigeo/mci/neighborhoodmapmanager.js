14bcdf1803c80b19d8848ffbf474a87d46d5e9c3
cbonas <57537066+cbonas@users.noreply.github.com>
2020-07-13 16:09:16 +0200

    Changement symbole type d'intervention (OSMA)

--- public/scripts/galigeo/mci/neighborhoodmapmanager.js
+++ public/scripts/galigeo/mci/neighborhoodmapmanager.js
@@ -162,6 +162,9 @@
         response.mission_ronde.features.forEach(f => {
           f.properties['marker-size'] = 'small';
           if (parseInt(f.properties.type_mission_id) == 6) {
+            if (parseInt(f.properties.motif_id) == 1) {
+              f.properties['marker-symbol'] = 'circle'; 
+            }
             f.properties['marker-symbol'] = 'triangle';
           }
           f.properties['marker-color'] = GGO.getColorForStatutMission(parseInt(f.properties.statut_mission));
