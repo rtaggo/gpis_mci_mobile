539ca31ea55b224f8ff2b3191aa631de86c67796
cbonas <57537066+cbonas@users.noreply.github.com>
2020-07-13 16:49:45 +0200

    Update neighborhoodmapmanager.js

--- public/scripts/galigeo/mci/neighborhoodmapmanager.js
+++ public/scripts/galigeo/mci/neighborhoodmapmanager.js
@@ -163,7 +163,7 @@
           f.properties['marker-size'] = 'small';
           if (parseInt(f.properties.type_mission_id) == 6) {
             if (parseInt(f.properties.motif_id) == 1) {
-              f.properties['marker-symbol'] = 'circle'; 
+              f.properties['marker-symbol'] = 'music'; 
             }
             else f.properties['marker-symbol'] = 'triangle';
           }
