35daf035d448302c1cdf2ab745966589762ebf39
cbonas <57537066+cbonas@users.noreply.github.com>
2020-07-15 17:02:54 +0200

    Update neighborhoodmapmanager.js

--- public/scripts/galigeo/mci/neighborhoodmapmanager.js
+++ public/scripts/galigeo/mci/neighborhoodmapmanager.js
@@ -165,7 +165,7 @@
             if (parseInt(f.properties.motif_id) == 1) {
               f.properties['marker-symbol'] = 'music'; 
             }
-            else f.properties['marker-symbol'] = 'school';
+            else f.properties['marker-symbol'] = 'pitch';
           }
           f.properties['marker-color'] = GGO.getColorForStatutMission(parseInt(f.properties.statut_mission));
           f.properties['description'] = f.properties.codesite;
