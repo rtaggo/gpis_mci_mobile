ba4958633f97fd1a629d5a27aad9e80dcd37be2a
cbonas <57537066+cbonas@users.noreply.github.com>
2020-07-16 16:21:51 +0200

    Update mapmanager.js

--- public/scripts/galigeo/mci/mapmanager.js
+++ public/scripts/galigeo/mci/mapmanager.js
@@ -267,10 +267,7 @@
       geojson.features.forEach(f => {
         f.properties['marker-size'] = 'small';
         f.properties['marker-symbol'] = f.properties['niveau_operationnel'];
-        if (f.properties['ronde_finalisee'] == 'true'){
-          f.properties['marker-color'] = this._getColorForRondesFin();
-        }
-        else f.properties['marker-color'] = this._getColorForNiveauOpe(f.properties['niveau_operationnel']);
+        f.properties['marker-color'] = this._getColorForNiveauOpe(f.properties['niveau_operationnel']);
         f.properties['description'] = f.properties.codesite;
       });
     },
