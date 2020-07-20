bd4450e0722daa4af8607ae80da8f03f82cf3b54
cbonas <57537066+cbonas@users.noreply.github.com>
2020-07-20 16:30:19 +0200

    Revert "test"
    This reverts commit e0f49d3424e11700b4e8d0bdbb026e68b7808ae0.

--- public/scripts/galigeo/galigeo.js
+++ public/scripts/galigeo/galigeo.js
@@ -59,7 +59,7 @@
     });
   };
 
-  GGO.SessionSummaryPrompt = function(title, container, patrouilleId) {
+  GGO.SessionSummaryPrompt = function(title, container) {
     container.append(`
         <section role="alertdialog" tabindex="0" aria-labelledby="prompt-heading-id" aria-describedby="prompt-message-wrapper" class="slds-modal slds-fade-in-open slds-modal_prompt" aria-modal="true">
           <div class="slds-modal__container">
@@ -103,7 +103,7 @@
       2. afficher les infos dans 'prompt-message-wrapper' $('#prompt-message-wrapper').empty()
       */
       $('#prompt-message-wrapper').empty();
-      const patrouillesUrl = `${self.baseRESTServicesURL}/bilan_vacation.php?patrouille=${patrouilleId}`;
+      const patrouillesUrl = `${self.baseRESTServicesURL}/bilan_vacation.php?patrouille=${self.patrouille}`;
       $.ajax({
         type: 'GET',
         url: patrouillesUrl,
