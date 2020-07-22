a0a8d340bb54499dd76778a38f76ee963c7d8b55
rtaggo <rteina@galigeo.com>
2020-07-22 11:28:56 +0200

    ajout gestion de cookie
    au demarrage, vidage du cookie 'gpis_forbidden_sites'

--- public/scripts/galigeo/galigeo.js
+++ public/scripts/galigeo/galigeo.js
@@ -1,14 +1,14 @@
 /* START GGO: put code below in galigeo.js */
-(function() {
+(function () {
   'use strict';
   var GGO = {
-    version: 'GGO.0.0.1'
+    version: 'GGO.0.0.1',
   };
 
   function expose() {
     var oldGGO = window.GGO;
 
-    GGO.noConflict = function() {
+    GGO.noConflict = function () {
       window.GGO = oldGGO;
       return this;
     };
@@ -30,7 +30,7 @@
     expose();
   }
 
-  GGO.SessionIssuePrompt = function(title, message, container) {
+  GGO.SessionIssuePrompt = function (title, message, container) {
     container.empty().append(`
         <section role="alertdialog" tabindex="0" aria-labelledby="prompt-heading-id" aria-describedby="prompt-message-wrapper" class="slds-modal slds-fade-in-open slds-modal_prompt" aria-modal="true">
           <div class="slds-modal__container">
@@ -47,7 +47,7 @@
         </section>
         <div class="slds-backdrop slds-backdrop_open"></div>
     `);
-    $('#appContainer footer > button.slds-button').click(function(e) {
+    $('#appContainer footer > button.slds-button').click(function (e) {
       GGO.disconnect();
       /*
       sessionStorage.clear();
@@ -56,10 +56,10 @@
       $('body').append(logoutForm);
       logoutForm.submit();
       */
-    })
+    });
   };
 
-  GGO.SessionSummaryPrompt = function(title, container, options) {
+  GGO.SessionSummaryPrompt = function (title, container, options) {
     container.append(`
         <section role="alertdialog" tabindex="0" aria-labelledby="prompt-heading-id" aria-describedby="prompt-message-wrapper" class="slds-modal slds-fade-in-open slds-modal_prompt" aria-modal="true" style="z-index: 10000;">
           <div class="slds-modal__container">
@@ -76,112 +76,109 @@
         </section>
         <div class="slds-backdrop slds-backdrop_open"></div>
     `);
-    $('#appContainer footer > button.slds-button').click(function(e) {
-      const what=$(this).data('what');
+    $('#appContainer footer > button.slds-button').click(function (e) {
+      const what = $(this).data('what');
       switch (what) {
         case 'end':
           let patrouille = self.galigeo.getPatrouille();
-          GGO.disconnect(
-            patrouille ? patrouille.id : undefined, 
-            {
-              'baseRESTServicesURL': '/services/rest/mci',
-              'fonction': 'fin'
-            }
-          );
+          GGO.disconnect(patrouille ? patrouille.id : undefined, {
+            baseRESTServicesURL: '/services/rest/mci',
+            fonction: 'fin',
+          });
           break;
         case 'return':
           $('.slds-modal').remove();
           $('.slds-backdrop').remove();
           break;
-      
+
         default:
           break;
       }
     });
-      /* TODO:
+    /* TODO:
       1. appel REST pour recupérer les infos
       2. afficher les infos dans 'prompt-message-wrapper' $('#prompt-message-wrapper').empty()
       */
-      let paramsArr = [];
-      if (options.patrouilleId) {
-        paramsArr.push(`patrouille_id=${options.patrouilleId}`);
-      } else {
-        paramsArr.push(`chef_groupe=${options.userName}`);
-        paramsArr.push(`chefs_groupe=${options.chefsGroupe}`)
-        
-      }
-      const vacationUrl = `${options.baseRESTServicesURL}/bilan_vacation.php?${paramsArr.join('&')}`;
-      $.ajax({
-        type: 'GET',
-        url: vacationUrl,
-        //dataType: 'json',
-        success: function(response) {
-          console.log(`Resume Patrouille Vacation response: `, response);
-          let type_cat_missions = [
-            {
-              type_cat : 1,
-              label: 'Rondes', 
-              nb: 0,
-            },
-            {
-              type_cat : 2,
-              label: 'Interventions', 
-              nb: 0,
-            },
-            {
-              type_cat : 3,
-              label: 'Activités dirigées', 
-              nb: 0,
-            },
-            {
-              type_cat : 4,
-              label: 'OPC', 
-              nb: 0,
-            }
-          ];
-          
-         
-         if (response.code === 200) {
-          response.bilan_vacation.forEach(b => {
-            let cat_miss = type_cat_missions.find(m => m.type_cat === b.type_categorie_mission);
+    let paramsArr = [];
+    if (options.patrouilleId) {
+      paramsArr.push(`patrouille_id=${options.patrouilleId}`);
+    } else {
+      paramsArr.push(`chef_groupe=${options.userName}`);
+      paramsArr.push(`chefs_groupe=${options.chefsGroupe}`);
+    }
+    const vacationUrl = `${options.baseRESTServicesURL}/bilan_vacation.php?${paramsArr.join('&')}`;
+    $.ajax({
+      type: 'GET',
+      url: vacationUrl,
+      //dataType: 'json',
+      success: function (response) {
+        console.log(`Resume Patrouille Vacation response: `, response);
+        let type_cat_missions = [
+          {
+            type_cat: 1,
+            label: 'Rondes',
+            nb: 0,
+          },
+          {
+            type_cat: 2,
+            label: 'Interventions',
+            nb: 0,
+          },
+          {
+            type_cat: 3,
+            label: 'Activités dirigées',
+            nb: 0,
+          },
+          {
+            type_cat: 4,
+            label: 'OPC',
+            nb: 0,
+          },
+        ];
+
+        if (response.code === 200) {
+          response.bilan_vacation.forEach((b) => {
+            let cat_miss = type_cat_missions.find((m) => m.type_cat === b.type_categorie_mission);
             if (cat_miss) {
               cat_miss.nb = b.nb_missions;
             }
           });
-          $('#prompt-message-wrapper').empty().append($(`
+          $('#prompt-message-wrapper')
+            .empty()
+            .append(
+              $(`
             <div class="slds-region_narrow" style="width:100%;">
               <dl class="slds-dl_horizontal">
                 ${type_cat_missions
                   //.filter(m => m.nb > 0) /* Si besoin de filtrage */
-                  .map(b => {
-                  return `<dt class="slds-dl_horizontal__label" style="width:50%;"><b>${b.label}</b>:</dt>
+                  .map((b) => {
+                    return `<dt class="slds-dl_horizontal__label" style="width:50%;"><b>${b.label}</b>:</dt>
                   <dd class="slds-dl_horizontal__detail" style="width:50%;">${b.nb}</dd>`;
-                }).join('')}
+                  })
+                  .join('')}
               </dl>
-            </div>`));
-         } else {
-           // mettre message d'erreur
-         }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          if (textStatus === 'abort') {
-            console.warn(`${vacationUrl} Request aborted`);
-          } else {
-            console.error(`Error for ${vacationUrl} request: ${textStatus}`, errorThrown);
-          }
-          
+            </div>`)
+            );
+        } else {
+          // mettre message d'erreur
         }
-      });
-    
-    
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        if (textStatus === 'abort') {
+          console.warn(`${vacationUrl} Request aborted`);
+        } else {
+          console.error(`Error for ${vacationUrl} request: ${textStatus}`, errorThrown);
+        }
+      },
+    });
   };
 
-  GGO.getRandomInteger = function(min, max) {
+  GGO.getRandomInteger = function (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   };
-  GGO.invertColor = function(hexTripletColor) {
+  GGO.invertColor = function (hexTripletColor) {
     var color = hexTripletColor;
     color = color.substring(1); // remove #
     color = parseInt(color, 16); // convert to integer
@@ -192,7 +189,7 @@
     return color;
   };
 
-  GGO.formatDistance = function(distanceKM) {
+  GGO.formatDistance = function (distanceKM) {
     if (distanceKM < 1) {
       return (distanceKM * 1000).toFixed(0) + ' m';
     } else {
@@ -206,10 +203,10 @@
     rdYlBu: ['#4874bf', '#228714', '#edeb2a', '#f99e38', '#ff0000', '#000000'],
     secteurs: ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9'],
     secteurs_voisinages: ['#d9d9d9', '#fccde5', '#b3de69', '#fdb462', '#80b1d3', '#fb8072', '#bebada', '#ffffb3', '#8dd3c7'],
-    rondes_fin:['#808080']
+    rondes_fin: ['#808080'],
   };
 
-  GGO.notifyNewMissionSound = function(code) {
+  GGO.notifyNewMissionSound = function (code) {
     if (code === 1) {
       var name_music = '/sounds/new_mission.mp3'; // new mission
     } else if (code === 2) {
@@ -218,9 +215,9 @@
     try {
       var music = new Audio(name_music);
       var p = music.play();
-      p.then(event => {
+      p.then((event) => {
         console.log(event);
-      }).catch(err => {
+      }).catch((err) => {
         console.log(err.message);
       });
     } catch (error) {
@@ -228,15 +225,15 @@
     }
   };
 
-  GGO.getDefaultColorPalette = function() {
+  GGO.getDefaultColorPalette = function () {
     return GGO.COLORPALETTES['rdYlBu'];
   };
 
-  GGO.getColorPalette = function(paletteName) {
+  GGO.getColorPalette = function (paletteName) {
     return GGO.COLORPALETTES[paletteName] || GGO.getDefaultColorPalette();
   };
 
-  GGO.getColorForStatutMission = function(statut) {
+  GGO.getColorForStatutMission = function (statut) {
     if (statut === 1) {
       return '#FFC100';
     } else if (statut === 2) {
@@ -246,7 +243,7 @@
     }
   };
 
-  GGO.getColorForStatutMissionLabel = function(statut) {
+  GGO.getColorForStatutMissionLabel = function (statut) {
     if (statut === 'En direction') {
       return '#FFC100';
     } else if (statut === 'Début') {
@@ -256,7 +253,7 @@
     }
   };
 
-  GGO.shadeHexColor = function(color, percent) {
+  GGO.shadeHexColor = function (color, percent) {
     /* 
       Percent: 
         > 0 => lighter
@@ -271,18 +268,18 @@
     return '#' + (0x1000000 + (Math.round((t - R) * p) + R) * 0x10000 + (Math.round((t - G) * p) + G) * 0x100 + (Math.round((t - B) * p) + B)).toString(16).slice(1);
   };
 
-  GGO.revokePatrouille = function(patrouilleId, options) {
+  GGO.revokePatrouille = function (patrouilleId, options) {
     if (typeof patrouilleId !== 'undefined') {
       const patrouillesUrl = `${options.baseRESTServicesURL}/liberer_patrouille.php?patrouille=${patrouilleId}`;
       $.ajax({
         type: 'GET',
         url: patrouillesUrl,
-        success: function(response) {
+        success: function (response) {
           if (typeof options.callback === 'function') {
             options.callback.apply(options.context);
           }
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
           if (textStatus === 'abort') {
             console.warn(`REVOKEPATROUILLE  ${patrouillesUrl} Request aborted`);
           } else {
@@ -291,12 +288,12 @@
           if (typeof options.callback === 'function') {
             options.callback.apply(options.context);
           }
-        }
+        },
       });
     }
   };
 
-  GGO.disconnect = function(patrouilleId, options) {
+  GGO.disconnect = function (patrouilleId, options) {
     sessionStorage.clear();
     if (typeof patrouilleId !== 'undefined') {
       if (options.fonction == 'deconnexion') {
@@ -304,38 +301,38 @@
         $.ajax({
           type: 'GET',
           url: patrouillesUrl,
-          success: function(response) {
+          success: function (response) {
             console.log(`Revoke Patrouille response: `, response);
             GGO.postLogoutForm();
           },
-          error: function(jqXHR, textStatus, errorThrown) {
+          error: function (jqXHR, textStatus, errorThrown) {
             if (textStatus === 'abort') {
               console.warn(`${patrouillesUrl} Request aborted`);
             } else {
               console.error(`Error for ${patrouillesUrl} request: ${textStatus}`, errorThrown);
             }
             GGO.postLogoutForm();
-          }
+          },
         });
       } else if (options.fonction === 'fin') {
         const patrouillesUrl = `${options.baseRESTServicesURL}/fin_vacation.php?patrouille=${patrouilleId}`;
         $.ajax({
           type: 'GET',
           url: patrouillesUrl,
-          success: function(response) {
+          success: function (response) {
             console.log(`Revoke Patrouille response: `, response);
             GGO.postLogoutForm();
           },
-          error: function(jqXHR, textStatus, errorThrown) {
+          error: function (jqXHR, textStatus, errorThrown) {
             if (textStatus === 'abort') {
               console.warn(`${patrouillesUrl} Request aborted`);
             } else {
               console.error(`Error for ${patrouillesUrl} request: ${textStatus}`, errorThrown);
             }
             GGO.postLogoutForm();
-          }
+          },
         });
-      } 
+      }
     } else if (typeof patrouilleId === 'undefined') {
       if (typeof options !== 'undefined') {
         if (options.fonction === 'fin') {
@@ -343,18 +340,18 @@
           $.ajax({
             type: 'GET',
             url: patrouillesUrl,
-            success: function(response) {
+            success: function (response) {
               console.log(`Revoke Patrouille response: `, response);
               GGO.postLogoutForm();
             },
-            error: function(jqXHR, textStatus, errorThrown) {
+            error: function (jqXHR, textStatus, errorThrown) {
               if (textStatus === 'abort') {
                 console.warn(`${patrouillesUrl} Request aborted`);
               } else {
                 console.error(`Error for ${patrouillesUrl} request: ${textStatus}`, errorThrown);
               }
               GGO.postLogoutForm();
-            }
+            },
           });
         } else {
           GGO.postLogoutForm();
@@ -370,7 +367,7 @@
     }
   };
 
-  GGO.postLogoutForm = function() {
+  GGO.postLogoutForm = function () {
     var logoutForm = $('<form action="/logout" />');
     $('body').append(logoutForm);
     logoutForm.submit();
@@ -384,7 +381,63 @@
     MISSIONCOMPLETED: 'missioncompleted',
     INVALIDATEMAPSIZE: 'invalidemapsize',
     NEIGHBORHOOD: 'neighborhood',
-    CLEARMISSIONMLOCATION: 'clearmissionlocation'
+    CLEARMISSIONMLOCATION: 'clearmissionlocation',
+  };
+
+  GGO.docCookies = {
+    getItem: function (sKey) {
+      if (!sKey || !this.hasItem(sKey)) {
+        return null;
+      }
+      return unescape(document.cookie.replace(new RegExp('(?:^|.*;\\s*)' + escape(sKey).replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*'), '$1'));
+    },
+    /**
+     * docCookies.setItem(sKey, sValue, vEnd, sPath, sDomain, bSecure)
+     *
+     * @argument sKey (String): the name of the cookie;
+     * @argument sValue (String): the value of the cookie;
+     * @optional argument vEnd (Number, String, Date Object or null): the max-age in seconds (e.g., 31536e3 for a year) or the
+     *  expires date in GMTString format or in Date Object format; if not specified it will expire at the end of session;
+     * @optional argument sPath (String or null): e.g., "/", "/mydir"; if not specified, defaults to the current path of the current document location;
+     * @optional argument sDomain (String or null): e.g., "example.com", ".example.com" (includes all subdomains) or "subdomain.example.com"; if not
+     * specified, defaults to the host portion of the current document location;
+     * @optional argument bSecure (Boolean or null): cookie will be transmitted only over secure protocol as https;
+     * @return undefined;
+     **/
+
+    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/.test(sKey)) {
+        return;
+      }
+      var sExpires = '';
+      if (vEnd) {
+        switch (typeof vEnd) {
+          case 'number':
+            sExpires = '; max-age=' + vEnd;
+            break;
+          case 'string':
+            sExpires = '; expires=' + vEnd;
+            break;
+          case 'object':
+            if (vEnd.hasOwnProperty('toGMTString')) {
+              sExpires = '; expires=' + vEnd.toGMTString();
+            }
+            break;
+        }
+      }
+      document.cookie = escape(sKey) + '=' + escape(sValue) + sExpires + (sDomain ? '; domain=' + sDomain : '') + (sPath ? '; path=' + sPath : '') + (bSecure ? '; secure' : '') + ';SameSite=Lax';
+    },
+    removeItem: function (sKey) {
+      if (!sKey || !this.hasItem(sKey)) {
+        return;
+      }
+      var oExpDate = new Date();
+      oExpDate.setDate(oExpDate.getDate() - 1);
+      document.cookie = escape(sKey) + '=; expires=' + oExpDate.toGMTString() + '; path=/';
+    },
+    hasItem: function (sKey) {
+      return new RegExp('(?:^|;\\s*)' + escape(sKey).replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\=').test(document.cookie);
+    },
   };
 })();
 /* end GGO: put code below in galigeo.js */
