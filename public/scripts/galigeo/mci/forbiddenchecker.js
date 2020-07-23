ccc638278f61982b44b4abf9f580312e3314b37b
rteina <rteina@galigeo.com>
2020-07-23 16:46:51 +0200

    Update forbiddenchecker.js
    add alertRestrictionsPrompt function

--- public/scripts/galigeo/mci/forbiddenchecker.js
+++ public/scripts/galigeo/mci/forbiddenchecker.js
@@ -42,6 +42,7 @@
               // update du cookie
               GGO.docCookies.setItem('gpis_forbidden_sites', JSON.stringify([...sites.map((s) => s.id), ...sites_set]), 31536e3, '/', null);
               // afficher la popup
+              /*
               GGO.AlertRestrictionsPrompt = function (title, container, options) {
                 container.append(`
                     <section role="alertdialog" tabindex="0" aria-labelledby="prompt-heading-id" aria-describedby="prompt-message-wrapper" class="slds-modal slds-fade-in-open slds-modal_prompt" aria-modal="true" style="z-index: 10000;">
@@ -93,7 +94,7 @@
                         <ul >
                           <li style="font-size :large" >Interdits<br></li> 
                           ${sites
-                            //.filter(m => m.nb > 0) /* Si besoin de filtrage */
+                            //.filter(m => m.nb > 0) // Si besoin de filtrage
                             .map((b) => {
                               if (b.type_restriction_id === 2) {
                                 return `<li >${b.nom}</li>`;
@@ -106,7 +107,7 @@
                         <ul>
                           <li style="font-size :large">Renforcés<br></li>
                           ${sites
-                            //.filter(m => m.nb > 0) /* Si besoin de filtrage */
+                            //.filter(m => m.nb > 0) // Si besoin de filtrage
                             .map((b) => {
                               if (b.type_restriction_id === 3) {
                                 return `<li >${b.nom}</li>`;
@@ -119,6 +120,8 @@
                   );
               };
               GGO.AlertRestrictionsPrompt('Nouvelles restrictions', $('#appContainer'));
+              */
+              self.alertRestrictionsPrompt('Nouvelles restrictions', $('#appContainer'), sites, options);
             }
           }
           self.await_before_recall();
@@ -132,6 +135,82 @@
           self.await_before_recall();
         },
       });
+    },
+    alertRestrictionsPrompt: function (title, container, sites, options) {
+      container.append(`
+        <section role="alertdialog" tabindex="0" aria-labelledby="prompt-heading-id" aria-describedby="prompt-message-wrapper" class="slds-modal slds-fade-in-open slds-modal_prompt" aria-modal="true" style="z-index: 10000;">
+          <div class="slds-modal__container">
+            <header class="slds-modal__header slds-theme_error slds-theme_alert-texture">
+              <h2 class="slds-text-heading_medium" id="prompt-heading-id">${title}</h2>
+            </header>
+            <div class="slds-modal__content slds-p-around_medium" id="prompt-message-wrapper">
+            </div>
+            <footer class="slds-modal__footer slds-theme_default">
+              <button class="slds-button slds-button_neutral" data-what="return">Fermer</button>
+            </footer>
+          </div>
+        </section>
+        <div class="slds-backdrop slds-backdrop_open"></div>
+      `);
+      $('#appContainer footer > button.slds-button').click(function (e) {
+        $('.slds-modal').remove();
+        $('.slds-backdrop').remove();
+      });
+      let type_lieu_restriction = [
+        {
+          type_cat: 'Site',
+          label: 'Sites',
+          nom: '',
+        },
+        {
+          type_cat: 'Arrondissement',
+          label: 'Arrondissements',
+          nom: '',
+        },
+        {
+          type_cat: 'Sous-Secteur',
+          label: 'Sous-secteurs',
+          nom: '',
+        },
+      ];
+      sites.forEach((b) => {
+        let cat_restr = type_lieu_restriction.find((m) => m.type_cat === b.type);
+        if (cat_restr) {
+          cat_restr.nom = b.nom;
+        }
+      });
+      $('#prompt-message-wrapper')
+        .empty()
+        .append(
+          $(`<div id="general">
+              <div id="interdits" style="width:50%; float:left">
+                <ul >
+                  <li style="font-size :large" >Interdits<br></li> 
+                  ${sites
+                    //.filter(m => m.nb > 0) /* Si besoin de filtrage */
+                    .map((b) => {
+                      if (b.type_restriction_id === 2) {
+                        return `<li >${b.nom}</li>`;
+                      }
+                    })
+                    .join('')}
+                </ul>
+              </div>
+              <div id="renforces" style="width:50% ; float:right">
+                <ul>
+                  <li style="font-size :large">Renforcés<br></li>
+                  ${sites
+                    //.filter(m => m.nb > 0) /* Si besoin de filtrage */
+                    .map((b) => {
+                      if (b.type_restriction_id === 3) {
+                        return `<li >${b.nom}</li>`;
+                      }
+                    })
+                    .join('')}
+                </ul>
+              </div>
+            </div>`)
+        );
     },
     await_before_recall: function () {
       setTimeout(() => {
